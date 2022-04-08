06b8b1cfffc557933fec33fc9d0eba6b0c2de6e0
Shahid Hassan <shaahid.xd@gmail.com>
2022-04-09 01:46:27 +0530

    feature(app): init app

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
